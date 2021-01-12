64f2a32d64e88e599f96457af906a807aa374c10
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-01-12 17:29:41 -0300

    States configured with Hooks API

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function SearchMovies(){
+function SearchMovies() {
+
+     // states for query and movies
+     const [query, setQuery] = useState('');
+     const [movies, setMovies] = useState([]) // Movies come with empty array
     
     const searchMovies = async (event) => {
         event.preventDefault();
         console.log("submitting");
 
-        const query = "Top gun"
+        /* Original hard-coded value used to test the API callback */
+        // const query = "Top Gun"
         
-        const url = `https://api.themoviedb.org/3/movie/550?api_key=503cd23467ddbca491d6127d9d19325b&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=503cd23467ddbca491d6127d9d19325b&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
             const response = await fetch(url);
             const data  = await response.json();
-            console.log(data);
+            setMovies(data.results);
+            console.log(data.results);
         }catch(err){
             console.error(err);
         }
@@ -23,8 +29,13 @@ export default function SearchMovies(){
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Typing a Name</label>
             <input className="input" type="text" name="query"
-              placeholder="i.e. Top Gun"/>
+              placeholder="i.e. Top Gun"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              />
             <button className="button" type="submit">Search</button>
         </form>
     )
 }
+
+export default SearchMovies;
